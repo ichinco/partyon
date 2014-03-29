8cab9d6a7d206b504eb1625ac448301e32b6c92d
Denise Ichinco <ichinco@gmail.com>
2014-03-29 11:11:35 -0400

    trips laid out

--- app/controllers/trip_controller.rb
+++ app/controllers/trip_controller.rb
@@ -1,4 +1,24 @@
 class TripController < ApplicationController
 
+  def index
+    @trips = Trip.all
+  end
 
+  def create
+    @trip = Trip.new(trip_parameters)
+    if @trip.save()
+      redirect_to trip_path(@trip)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @trip = Trip.find(params[:id])
+  end
+
+  private
+  def trip_parameters
+    params.require(:trip).permit(:title,:location,:length,:date)
+  end
 end
