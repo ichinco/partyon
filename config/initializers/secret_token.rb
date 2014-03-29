176b18b47b1c763f601f6b7c29fa3de31eee9849
Denise Ichinco <ichinco@gmail.com>
2014-03-29 09:36:45 -0400

    reborn

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Partyon::Application.config.secret_key_base = 'd2bdc1b26d2497e3aeb3d36e909be38cd6aa423344faff0f961230b47f964e20deeaebfc099a410658975421498cc0c3e762302ea0edaf01e0c7983ac99c6bea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Partyon::Application.config.session_store :cookie_store, key: '_partyon_session'
