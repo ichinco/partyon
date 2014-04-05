d4fdcc92aa0fa3a53493147034745f453e053a99
Denise Ichinco <ichinco@gmail.com>
2014-04-05 16:59:33 -0400

    added validation for cost creation

--- app/controllers/cost_controller.rb
+++ app/controllers/cost_controller.rb
@@ -1,9 +1,15 @@
 class CostController < ApplicationController
+  include ActionView::Helpers::TextHelper
+
   before_action :get_trip
   before_action :authenticate_user!
 
   def get_trip
     @trip = Trip.find(params[:trip_id])
+  end
+
+  def new
+    @cost = Cost.new()
   end
 
   def create
@@ -12,6 +18,7 @@ class CostController < ApplicationController
     if @cost.save
       redirect_to trip_cost_index_path(@trip)
     else
+      flash[:alert] = "#{pluralize(@cost.errors.count,"error")} prevented this cost from being created."
       render 'new'
     end
   end
@@ -19,10 +26,12 @@ class CostController < ApplicationController
   def index
     @costs = Cost.where(trip_id: @trip.id)
     @total = @costs.inject(0) do |sum, cost|
-      if cost.actual_amount.nil?
+      if cost.actual_amount.nil? and not cost.estimated_amount.nil?
         sum + cost.estimated_amount
-      else
+      elsif not cost.estimated_amount.nil?
         sum + cost.actual_amount
+      else
+        sum
       end
     end
 
