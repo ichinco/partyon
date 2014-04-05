64ba139ccb7ac44357627a2801f78ef853a8bf86
Denise Ichinco <ichinco@gmail.com>
2014-04-05 17:08:36 -0400

    do not create empty records for omitted fields in activity

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -5,6 +5,14 @@ class Activity < ActiveRecord::Base
   validates :name, presence: true
   validates :location, presence: true
 
-  accepts_nested_attributes_for :costs
-  accepts_nested_attributes_for :schedules
+  accepts_nested_attributes_for :costs, reject_if: :estimated_cost_blank
+  accepts_nested_attributes_for :schedules, reject_if: :day_start_time_blank
+
+  def estimated_cost_blank(a)
+    a[:estimated_cost].blank?
+  end
+
+  def day_start_time_blank(a)
+    a[:day].blank? or a[:start_time].blank?
+  end
 end
