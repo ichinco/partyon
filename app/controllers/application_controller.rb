0bf20a0eccee318afd8e8751975b86c21815e07a
Denise Ichinco <ichinco@gmail.com>
2014-04-09 17:37:11 -0400

    added phone to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,8 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
-    devise_parameter_sanitizer.for(:account_update) << :name
+    devise_parameter_sanitizer.for(:sign_up) << :phone
+    devise_parameter_sanitizer.for(:account_update) << :phone
   end
 
   def after_sign_in_path_for(resource)
