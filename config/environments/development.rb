709f98b152454d68cd57f551f00d094ebc251ec6
Denise Ichinco <ichinco@gmail.com>
2014-04-11 10:50:21 -0400

    progress

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,4 +28,5 @@ Partyon::Application.configure do
   config.assets.debug = true
 
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
