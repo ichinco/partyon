3ca99cc699e669a55513bf4aed62e0f8da2e733a
Denise Ichinco <ichinco@gmail.com>
2014-04-26 15:13:29 -0400

    got single user responsibility added

--- app/assets/javascripts/cost.js.coffee
+++ app/assets/javascripts/cost.js.coffee
@@ -2,12 +2,22 @@
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
 
+costMultiselectChange = (element, checked) ->
+  $.ajax({
+    url:element.parent().data("url"),
+    method:"post",
+    data:{
+      "user_id" : element.val(),
+      "owes" : checked
+    }
+  })
+
 multiselectReady = () ->
   $('.multiselect').multiselect({
     includeSelectAllOption:true,
     selectAllText: "Everyone",
     selectAllValue:"multiselect-all"
-
+    onChange: costMultiselectChange
   });
 
 $(document).ready(multiselectReady)
--- app/controllers/cost_user_controller.rb
+++ app/controllers/cost_user_controller.rb
@@ -3,24 +3,21 @@ class CostUserController < ApplicationController
   before_action :pretrip
   before_action :require_trip_admin
 
-  def destroy
-    @cost_user = CostUser.find(params[:id])
-    if @cost_user.destroy
-      redirect_to trip_cost_index_path(@trip)
-    end
-  end
-
   def create
-    @cost_user = CostUser.new(cost_user_params)
-    @cost_user.cost_id = params[:cost_id]
-    @cost_user.trip_id = params[:trip_id]
-    if @cost_user.save
-      redirect_to trip_cost_index_path(@trip)
+    @successful = false
+    if params[:owes] == "true"
+      @cost_user = CostUser.new()
+      @cost_user.cost_id = params[:cost_id]
+      @cost_user.user_id = params[:user_id]
+      @cost_user.trip_id = params[:trip_id]
+      @successful = @cost_user.save
+    else
+      @cost_user = CostUser.where(:cost_id=> params[:cost_id], :trip_id=>params[:trip_id])
+      @cost_user.each do |u|
+        @successful = @successful && u.destroy
+      end
     end
-  end
 
-  private
-  def cost_user_params
-    params.require(:cost_user).permit(:user_id)
+    render json:{"success" => @successful}
   end
 end
